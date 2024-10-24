152ed849464048c32d64ff05eb4e6decf3474df7
Ade <79329667+adedolapoadegboye@users.noreply.github.com>
2024-10-23 23:30:51 -0700

    initial commit

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x = 0;
+    int y = 1;
+
+    int *px = &x;
+    int *py = &y;
+
+    printf("%d %d %d %d\n", x, y, *px, *py);
+
+    printf("Please enter a number: ");
+    scanf("%d", px);
+
+    printf("%d %d %d %d\n", x, y, *px, *py);
+
+    printf("Please enter a number: ");
+    scanf("%d", py);
+
+    printf("%d %d %d %d\n", x, y, *px, *py);
+
+    return 0;
+}
