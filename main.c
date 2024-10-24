16c95896f4aa1e0419f2490c0c6232a587e50ef2
Ade <79329667+adedolapoadegboye@users.noreply.github.com>
2024-10-24 00:05:33 -0700

    initial commit

--- main.c
+++ main.c
@@ -24,5 +24,20 @@ int main()
 
     printf("%d %d", x + y, *px + *py);
 
+    printf("\nAddress of x: %p\n", (void *)&x);
+    printf("Address of y: %p\n", (void *)&y);
+    printf("Address of px: %p\n", (void *)&px);
+    printf("Address of py: %p\n", (void *)&py);
+
+    printf("Size of x: %lu bytes\n", sizeof(x));
+    printf("Size of y: %lu bytes\n", sizeof(y));
+    printf("Size of px: %lu bytes\n", sizeof(px));
+    printf("Size of py: %lu bytes\n", sizeof(py));
+
+    printf("Value of x: %d\n", x);
+    printf("Value of y: %d\n", y);
+    printf("Value of px: %d\n", *px);
+    printf("Value of py: %d\n", *py);
+
     return 0;
 }
